238666ff3a6a5b1f7845c9c0468079906fa6e71a
Iman Anooshehpour <imananoosheh@gmail.com>
2022-02-27 20:31:37 -0800

    developed basic functions

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const video = document.querySelector("video");
+const textDiv = document.querySelector("[data-text]");
 
 async function setup() {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-}
-video.srcObject = stream;
+  video.srcObject = stream;
 
-video.addEventListener("playing", async () => {
-  const worker = Tesseract.createWorker();
-  await worker.load();
-  await worker.loadLanguage("eng");
-  await worker.initialize();
+  video.addEventListener("playing", async () => {
+    const worker = Tesseract.createWorker();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize();
 
-  const canvas = document.createElement("canvas");
-  canvas.width = video.width;
-  canvas.height = video.height;
+    const canvas = document.createElement("canvas");
+    canvas.width = video.width;
+    canvas.height = video.height;
 
-  document.addEventListener("keypress", async (e) => {
-    if (e.code !== "Space") return;
-    canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
-    const obj = await worker.recognize(canvas)
-    console.log(obj)
+    document.addEventListener("keypress", async (e) => {
+      console.time();
+      if (e.code !== "Space") return;
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
+      const {
+        data: { text },
+      } = await worker.recognize(canvas);
+      console.log(text);
+      console.timeEnd();
+
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance(text.replace(/\s/g, " "))
+      );
+      textDiv.textContent = text;
+    });
   });
-});
+}
+setup();
