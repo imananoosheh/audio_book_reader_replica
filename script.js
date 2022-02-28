031a751a6b9972c2ff97d870e54ab3ea278f6790
Iman Anooshehpour <imananoosheh@gmail.com>
2022-02-27 19:20:09 -0800

    initialize Audio Book Replica repo

---
+++ script.js
@@ -0,0 +1,24 @@
+const video = document.querySelector("video");
+
+async function setup() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+}
+video.srcObject = stream;
+
+video.addEventListener("playing", async () => {
+  const worker = Tesseract.createWorker();
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize();
+
+  const canvas = document.createElement("canvas");
+  canvas.width = video.width;
+  canvas.height = video.height;
+
+  document.addEventListener("keypress", async (e) => {
+    if (e.code !== "Space") return;
+    canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
+    const obj = await worker.recognize(canvas)
+    console.log(obj)
+  });
+});
